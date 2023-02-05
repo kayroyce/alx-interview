803c8df56087b42f2334e966ecb86bde7d42440d
kayroyce <kayroyce2005@gmail.com>
2023-02-05 21:06:17 +0100

    code check

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -5,7 +5,7 @@ Challenge:  You have n number of locked boxes in front of you.
             Each box is numbered sequentially from 0 to n - 1
             and each box may contain keys to the other boxes.
 What is required: To Write a method that determines if all the 
-boxes can be opened.
+                  boxes can be opened.
 """
 
 
